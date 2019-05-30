9df8e6a2982013dc4b6ef8ce582910c233c03243
dmy <>
2019-05-30 15:14:28 +0800

    新建一个订单管理View

---
+++ ZLead/Main/Shop/View/ZLOrderManageView.m
@@ -0,0 +1,34 @@
+//
+//  ZLOrderManageView.m
+//  ZLead
+//
+//  Created by dmy on 2019/5/30.
+//  Copyright © 2019 Beijing tai chi HuaQing information systems co., LTD. All rights reserved.
+//
+
+#import "ZLOrderManageView.h"
+#import "ZLOrderListCell.h"
+
+@interface ZLOrderManageView ()
+
+@end
+
+@implementation ZLOrderManageView
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupViews];
+    }
+    return self;
+}
+
+- (void)setupViews {
+    self.orderListTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kScreenWith, kScreenHeight) style:UITableViewStylePlain];
+    self.orderListTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.orderListTableView.backgroundColor = [UIColor clearColor];
+    [self.orderListTableView registerClass:[ZLOrderListCell class] forCellReuseIdentifier:@"ZLOrderListCell"];
+    [self addSubview:self.orderListTableView];
+}
+
+@end
