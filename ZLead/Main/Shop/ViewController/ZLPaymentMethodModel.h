1dcafdf29d5b258a8e20f03a04ca27a306a5dd77
dmy <>
2019-05-31 11:08:05 +0800

    添加一个支付方式model

---
+++ ZLead/Main/Shop/ViewController/ZLPaymentMethodModel.h
@@ -0,0 +1,18 @@
+//
+//  ZLPaymentMethodModel.h
+//  ZLead
+//
+//  Created by dmy on 2019/5/30.
+//  Copyright © 2019 Beijing tai chi HuaQing information systems co., LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZLPaymentMethodModel : NSObject
+@property (nonatomic, assign) BOOL isSelected;
+@property (nonatomic, strong) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZLead/Main/Shop/ViewController/ZLPaymentMethodModel.m
@@ -0,0 +1,13 @@
+//
+//  ZLPaymentMethodModel.m
+//  ZLead
+//
+//  Created by dmy on 2019/5/30.
+//  Copyright © 2019 Beijing tai chi HuaQing information systems co., LTD. All rights reserved.
+//
+
+#import "ZLPaymentMethodModel.h"
+
+@implementation ZLPaymentMethodModel
+
+@end
