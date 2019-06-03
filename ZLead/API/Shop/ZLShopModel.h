85675645ca7a4b7025d9fd726c70a7daadba9c5a
dmy <>
2019-06-03 11:13:04 +0800

    新建一个ZLShopModel

---
+++ ZLead/API/Shop/ZLShopModel.h
@@ -0,0 +1,17 @@
+//
+//  ZLShopModel.h
+//  ZLead
+//
+//  Created by dmy on 2019/6/3.
+//  Copyright © 2019 Beijing tai chi HuaQing information systems co., LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZLShopModel : NSObject
+@property (nonatomic, strong) NSString *shopName;
+@end
+
+NS_ASSUME_NONNULL_END
